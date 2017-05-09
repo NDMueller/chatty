b59c84426b6babf621e2d251943b218744ad229d
NDMueller <Nicholas@Nicholass-MacBook-Pro.local>
2017-05-09 15:24:48 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+var express =require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+var messages =[
+  {
+    id: 1,
+    title: 'Spiderman',
+    rating: 7
+  },
+  {
+    id: 2,
+    title: 'Spiderman 2',
+    rating: 7
+  }
+]
+app.listen(3000,function(){
+  console.log('listening on 3000')
+})
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({messages: messages});
+});
+app.post("/messages", function(request, response, next) {
+	messages.push({userName: request.body.userName, message: request.body.message, time: new Date()});
+	response.status(200).json({messages: messages})
+});
